2470ffbce4d4005c7085d6b879cc261a76f28ab7
mominramjan07 <mominramjan786786@gmail.com>
2025-10-08 15:25:16 +0530

    Updated student form and bonafide certificate

--- VMC/settings.py
+++ VMC/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-dev-key')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
