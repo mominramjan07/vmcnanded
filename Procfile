969b67d893bfb63eb10e6b2d370e9ea32197044f
mominramjan07 <mominramjan786786@gmail.com>
2025-10-08 11:57:11 +0530

    Add all project files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn VMC.wsgi:application --bind 0.0.0.0:$PORT
---
+++ VMC/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+     # 🔹 Authentication URLs
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='user_login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='user_login'), name='user_logout'),
+    path('', include('certificates.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
